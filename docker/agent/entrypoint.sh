ac8e3a8954e99eb9689b338751b97ee90a0ba83c
litnimax <litnimaxster@gmail.com>
2022-03-18 22:40:59 +0300

    Sleep fix

--- docker/agent/entrypoint.sh
+++ docker/agent/entrypoint.sh
@@ -6,6 +6,6 @@ tmux new-window -t dev:1 -n api
 tmux send-keys salt-api C-m
 tmux new-window -t dev:2 -n minion
 sleep 3 # Wait for API to get up.
-tmux send-keys sleep 2 && salt-minion C-m
+tmux send-keys salt-minion C-m
 # Sleep many many days :-)
 exec sleep 99999d
